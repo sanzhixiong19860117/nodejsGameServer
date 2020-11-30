82e1a1b8f249105c76ac3c4acb20b018f012fe64
joy <315570835@qq.com>
2020-11-30 22:39:36 +0800

    增加解析的管理模块proto_mgr.js

--- node_game_server/netbus/proto_mgr.js
+++ node_game_server/netbus/proto_mgr.js
@@ -4,6 +4,14 @@ const netbus = require("./netbus");
 
 const proto_mgr = {};//表
 
+const decoders = {};//解码集合
+const encoders = {};//编码结合
+
+//key的操作
+function get_key(stype,ctype){
+    return stype*65535 + ctype;
+}
+
 //json打包操作
 //stype 服务器的号码
 //ctype 对应的功能号码
@@ -26,9 +34,69 @@ function _json_decode(jsonbuf){
     }
     return jsonObj;
 }
+//end
 
+//二进制
+function encode_cmd(proto_type,stype,ctype,body){
+    //第一步算总长度
+    let buf = null;
+    if(proto_type == netbus.PROTO_JSON){
+        //如果是json
+        buf = _json_encode(stype,ctype,body);
+        log.info("encode_cmd=",proto_type,buf);
+        return buf;
+    }
+    //获得唯一的key
+    let key = get_key(stype,ctype);
+    if(!encoders[key]){
+        return;
+    }
+    buf = encoders[key](body);
+    return buf;
+}
 
+//解码
+function decode_cmd(proto_type,body){
+    let buf = null;
+    if(proto_type == netbus.PROTO_JSON){
+        buf = _json_decode(body)
+        return buf;
+    }
+    let stype = body.readUInt16LE(0);//从0开始读取
+    let ctype = body.readUInt16LE(2);//从第二个开始读取
+    let key  = get_key(stype,ctype);
 
-proto_mgr.json_decode = _json_decode;
-proto_mgr.json_encode = _json_encode;
+    //没有找到
+    if(decoders[key]){
+        return null;
+    }
+    buf = decoders[key](body)
+    return buf;
+}
+
+//二进制注册
+function reg_buf_encoder(stype,ctype,encode_fun){
+    let key = get_key(stype,ctype);
+    if(encoders[key]){
+        log.warn("proto_mgr.reg_buf_encoder is have key",key);
+        return;
+    }
+    encoders[key] = encode_fun;
+}
+
+//解码
+function reg_buf_decoder(stype,ctype,encode_fun) {
+    let key = get_key(stype,ctype);
+    if(decoders[key]){
+        log.warn("proto_mgr.reg_buf_decoder is have key",key);
+        return;
+    }
+    decoders[key] = encode_fun;
+}
+
+proto_mgr.decode_cmd = decode_cmd;
+proto_mgr.encode_cmd = encode_cmd;
+
+proto_mgr.reg_buf_decoder = reg_buf_decoder;
+proto_mgr.reg_buf_encoder = reg_buf_encoder;
 module.exports = proto_mgr;
--- node_game_server/test/text_protoMgr.js
+++ node_game_server/test/text_protoMgr.js
@@ -8,9 +8,69 @@ let data = {
     upasd:"123456",
 };
 
-let json = proto_mgr.json_encode(1,1,data);
+let json = proto_mgr.encode_cmd(netbus.PROTO_JSON,1,1,data);
 log.info(json)
 log.error("length = ",json.length);
 
-let json_str = proto_mgr.json_decode(json);
+let json_str = proto_mgr.decode_cmd(netbus.PROTO_JSON,json);
 log.info(json_str);
+
+//二进制 打包
+function encode_cmd_1_1(body) {
+    let stype = 1;
+    let ctype = 1;
+    //整个长度=服务号+消息号+数据1的长度+数据2的长度+2+2;
+    let total_len = 2+2+body.uname.length+body.upasd.length+2+2;
+    let buf = Buffer.allocUnsafe(total_len);//分配内存
+    buf.writeInt16LE(stype,0);//写入到内存中0,1
+    buf.writeInt16LE(ctype,2);//2,3
+    //写入对应的数据
+    buf.writeInt16LE(body.uname.length,4);//4开始
+    buf.write(body.uname,6);
+
+    let offset = 6 + body.uname.length;
+    buf.writeInt16LE(body.upasd.length,offset);
+    buf.write(body.upasd,offset+2);
+    return buf;
+}
+
+//解析二进制
+function decode_cmd_1_1(cmd_buf){
+    let stype = 1;
+    let ctype = 1;
+    let uname_len = cmd_buf.readInt16LE(4);
+    if((uname_len+2+2+2)>cmd_buf.length){
+        return null;
+    }
+
+    let uname = cmd_buf.toString("utf8",6,6+uname_len);
+    if(!uname){
+        return null;
+    }
+
+    let offset = 6 + uname_len;
+    let upwd_len = cmd_buf.readInt16LE(offset);
+    if((upwd_len + 2 + offset)>cmd_buf.length){
+        return null;
+    }
+
+    let upwd = cmd_buf.toString("utf8",offset + 2,offset + 2 + upwd_len);
+    let cmd = {
+        0:1,
+        1:1,
+        2:{
+            "uname":uname,
+            "upwd":upwd,
+        }
+    };
+    return cmd;
+}
+
+proto_mgr.reg_buf_encoder(1,1,encode_cmd_1_1);
+proto_mgr.reg_buf_decoder(1,1,decode_cmd_1_1);
+
+let proto_cmd_buf = proto_mgr.encode_cmd(netbus.PROTO_BUF,1,1,data);
+log.info(proto_cmd_buf);
+log.error(proto_cmd_buf.length);
+cmd = proto_mgr.decode_cmd(netbus.PROTO_BUF,proto_cmd_buf);
+log.info(cmd);
